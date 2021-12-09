acf0395ad2184ff7b4d1f26dd605e2701a8ee4bd
Peter Taylor <me@et1.uk>
2021-12-09 20:34:53 +0000

    forgot a bracket

--- docker/cron_script.sh
+++ docker/cron_script.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 if [ -e /changed ]; then
-    timeSinceMod=$(($(date +%s) - $(date +%s -r /changed))
+    timeSinceMod=$(($(date +%s) - $(date +%s -r /changed)))
     if [ $timeSinceMod -gt 600 ]; then
         python -m hstp -i /input -o /output
         rm /changed
