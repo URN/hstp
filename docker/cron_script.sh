1c2381fd3f0ede3553c2d2823f675635815ab40e
Peter Taylor <me@et1.uk>
2021-12-10 15:09:53 +0000

    added logging and fixed a script typo

--- docker/cron_script.sh
+++ docker/cron_script.sh
@@ -3,9 +3,10 @@
 if [ -e /changed ]; then
     timeSinceMod=$(($(date +%s) - $(date +%s -r /changed)))
     if [ $timeSinceMod -gt 600 ]; then
-        if [ ! -e /active]; then
+        if [ ! -e /active ]; then
             touch /active
-            python -m hstp -i /input -o /output
+            date >> /log
+            python -m hstp -i /input -o /output &>> /log
             rm /changed /active
         fi
     fi
