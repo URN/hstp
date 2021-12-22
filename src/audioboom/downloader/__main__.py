58c74788eef46449e8934bf75b097701b72eddfc
Peter Taylor <info@emersont1.co.uk>
2021-12-22 00:07:19 +0000

    Added to poetry file

--- src/audioboom/downloader/__main__.py
+++ src/audioboom/downloader/__main__.py
@@ -2,21 +2,25 @@ import argparse
 
 import audioboom
 
-parser = argparse.ArgumentParser(
-    prog='python -m audioboom-downloader',
-    description='Download Audioboom podcasts'
-)
+def main():
+    parser = argparse.ArgumentParser(
+        prog='python -m audioboom-downloader',
+        description='Download Audioboom podcasts'
+    )
 
-parser.add_argument("id", help="The ID of the audioboom channel")
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
-parser.add_argument("-o", "--output", help="output directory", default=".")
+    parser.add_argument("id", help="The ID of the audioboom channel")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("-o", "--output", help="output directory", default=".")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-c = audioboom.Channel(args.id)
+    c = audioboom.Channel(args.id)
 
-c.get_episodes()
-c.get_playlists()
+    c.get_episodes()
+    c.get_playlists()
 
-c.save(args.output)
+    c.save(args.output)
+
+if __name__ == "__main__":
+    main()
--- src/hstp/__main__.py
+++ src/hstp/__main__.py
@@ -3,33 +3,37 @@ import os
 
 import hstp
 
+def main():
 
-parser = argparse.ArgumentParser(
-    prog='python -m hstp',
-    description='Build an '
-)
+    parser = argparse.ArgumentParser(
+        prog='python -m hstp',
+        description='Build an '
+    )
 
-parser.add_argument("-v", "--verbose", help="increase output verbosity",
-                    action="store_true")
-parser.add_argument("-i", "--input", help="input directory")
-parser.add_argument(
-    "-o",
-    "--output",
-    help="output directory",
-    default="../out")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity",
+                        action="store_true")
+    parser.add_argument("-i", "--input", help="input directory")
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="output directory",
+        default="../out")
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-i = hstp.Info()
+    i = hstp.Info()
 
-if not args.input or (not os.path.isdir(args.input)):
-    i.error("Input directory does not exist")
-    exit(1)
+    if not args.input or (not os.path.isdir(args.input)):
+        i.error("Input directory does not exist")
+        exit(1)
 
-if not args.output or (not os.path.isdir(args.input)):
-    i.error("Input directory does not exist")
-    exit(1)
+    if not args.output or (not os.path.isdir(args.input)):
+        i.error("Input directory does not exist")
+        exit(1)
 
-r = hstp.Reader(i, args.input)
-r.load_podcasts()
-r.save(args.output)
+    r = hstp.Reader(i, args.input)
+    r.load_podcasts()
+    r.save(args.output)
+
+if __name__ == "__main__":
+    main()
